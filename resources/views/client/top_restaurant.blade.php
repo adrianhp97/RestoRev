9d8ee45ab714980c15b500ba0952893d45494368
Adrian Hartarto Pramudita <13515091@std.stei.itb.ac.id>
2018-03-25 11:14:46 +0700

    change top_restaurant

--- resources/views/client/top_restaurant.blade.php
+++ resources/views/client/top_restaurant.blade.php
@@ -3,14 +3,14 @@
     <div class="container">
         <div class="b_center_title">
             <h2>Top Restaurant</h2>
-            <p>We Are A Creative Digital Agency. Focused on Growing Brands Online</p>
+            <p>This week top restaurant</p>
         </div>
         <div class="l_news_inner">
             <div class="row">
             @foreach($topRestaurant as $restaurant)
                 <div class="col-lg-4 col-md-6">
                     <div class="l_news_item">
-                        <div class="l_news_img"><a href="{{ URL::route('client-review', (string)$restaurant->restaurant_id) }}"><img class="img-fluid" src="{{ URL::asset('storage/restaurant_img/' . $restaurant['name']) }}" alt=""></a></div>
+                        <div class="l_news_img"><a href="{{ URL::route('client-review', (string)$restaurant->restaurant_id) }}"><img class="img-fluid" src="{{ URL::asset('storage/restaurant_img/' . $restaurant->name) }}" alt=""></a></div>
                         <div class="l_news_content">
                             <a href="{{ URL::route('client-review', (string)$restaurant->restaurant_id) }}"><h4>{{ $restaurant->name }}</h4></a>
                             <p style="word-break: break-all">{{  substr(strip_tags($restaurant->desc,'<pre>,<code>'),0,100) }}{{ strlen(strip_tags($restaurant->desc)) > 100 ? "..." : "" }}.</p>
